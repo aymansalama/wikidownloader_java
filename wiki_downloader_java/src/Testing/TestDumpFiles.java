cd738c2905aa8ae004fe62a98c589a2ecfe4c22b
Yap Cheng Yee <32452451+Ycy97@users.noreply.github.com>
2018-12-03 19:16:42 +0800

    TestCaseForHTML_URL
    Skeletal structure for Kesaven's case for directing user to the right website using the URL

---
+++ wiki_downloader_java/src/Testing/TestDumpFiles.java
@@ -0,0 +1,24 @@
+package Testing;
+
+public class TestDumpFiles {
+
+    public static void main(String[] args)
+    {
+        String [] urlName = new String[10];
+
+    }
+
+    private void testUrl(String[] obtainedURL)
+    {
+        int i;
+        String result;
+
+        for(i =0 ; i < obtainedURL.length ;i++)
+        {
+            result = obtainedURL[i];
+            //and then run the function and pass result as an argument
+        }
+    }
+}
+
+
