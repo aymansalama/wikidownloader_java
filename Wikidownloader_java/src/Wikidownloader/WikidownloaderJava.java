d5227ae79987c5cd762da7dabcda45c5d1f07da2
Bhavish Doobaree <bdoobaree@gmail.com>
2018-12-03 12:12:43 +0800

    timestamps module update

--- Wikidownloader_java/src/Wikidownloader/WikidownloaderJava.java
+++ Wikidownloader_java/src/Wikidownloader/WikidownloaderJava.java
@@ -1,10 +1,26 @@
 package Wikidownloader;
 
+import java.io.File;
+import java.net.URL;
+
+
+
 public class WikidownloaderJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	}
+	
+	public static void arrayTimestamps(url_received)
+	{
+		String timeStampsArray[];
+		String url_wiki;
+		
+		url_wiki = url_received;
+		
+		
+		
 	}
 
 }
