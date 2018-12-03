6aeac8fe3154b5ce984e9a96dc0b76d407b25ff5
Wezuri <wes_peh@hotmail.com>
2018-12-03 16:03:22 +0800

    Deleted test functions again

--- wiki_downloader_java/src/application/Main.java
+++ wiki_downloader_java/src/application/Main.java
@@ -14,38 +14,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-//			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-//			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
 			
-			
-			// test the function
-			
-			String wikidumps="https://dumps.wikimedia.org";
-			
-			String filter="/nlwiktionary/20181201/nlwiktionary-20181201-categorylinks.sql.gz";
-			
-			String link = wikidumps+filter;
-			File out;
-			
-		
-			
-			
-			// directorychooser is used to get the path for download location
-			DirectoryChooser chooser = new DirectoryChooser();
-			File  file =chooser.showDialog(primaryStage);
-			
-			// filepath stores the path for the download location
-			String filepath = file.getAbsolutePath();
-			
-			out= new File(filepath+"/dumps.sql.gz");
-			
-		// download the file
-			new Thread(new Download(link,out)).start();
-			
-			
+
+					
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
