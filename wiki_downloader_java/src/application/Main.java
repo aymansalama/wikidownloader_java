78cd83ba3d0a43fb562d63612b58ed69f9f2f7b0
Mohamed Elsayed <melsayed.se@gmail.com>
2018-12-04 21:15:08 +0800

    get all completed projects list
    This function allows us to get an array list with all the names of the projects that were completely dumped and ready to be downloaded.

--- wiki_downloader_java/src/application/Main.java
+++ wiki_downloader_java/src/application/Main.java
@@ -1,6 +1,8 @@
 package application;
 	
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 import javafx.stage.DirectoryChooser;
@@ -8,6 +10,13 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 
 public class Main extends Application {
@@ -27,18 +36,53 @@ public class Main extends Application {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		launch(args);
 		
-		//exmaple to get the html characters
-		HtmlGet Html = new HtmlGet("https://www.youtube.com/");
-		Html.run();
+		String link = "https://dumps.wikimedia.org/backup-index.html";
 		
-		String result = Html.get_result();
+		ArrayList<String> namesList = getListOfProjects(link);
 		
-		System.out.println(result);
+		for(int i = 0; i < namesList.size(); i++) {
+			if(namesList.get(i).equals(""))
+				namesList.remove(i);
+		}
 		
+		for(int i = 0; i < namesList.size(); i++) {
+			System.out.println(namesList.get(i));
+		}
+		System.out.println("Total size is: " + namesList.size());
+		
+//		//example to get the HTML characters
+//		HtmlGet Html = new HtmlGet("https://dumps.wikimedia.org/backup-index.html");
+//		Html.run();
+//		
+//		String result = Html.get_result();
+//		
+//		System.out.println(result);	
+
 	}
 	
+	public static ArrayList<String> getListOfProjects(String linkToWebsite) throws IOException {
+		
+		/*
+		 * @param linkToWebsite is just a link to a web-page in the form of a String
+		 * @return This function will return an ArrayList with all the Text elements of an <a href http://...> ... </a> attribute in a page 
+		 */
+		
+		ArrayList<String> arrayList = new ArrayList<String>();
+		Document doc = Jsoup.connect(linkToWebsite).get();
+		
+		Elements classElement = doc.getElementsByTag("li");
+		
+		int i = 0;
+		
+		for(Element src: classElement) {
+			if(src.toString().contains("Dump complete")) {
+				arrayList.add(src.select("a[href]").text());
+			}
+		}
+		return arrayList;
+	}
 	
 }
