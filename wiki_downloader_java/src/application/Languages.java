5446178609a2edad347fbbe10f1ce28e435cf7b8
amirulumar <amirulumar1230@gmail.com>
2018-12-06 00:57:28 +0800

    Update Languages.java

--- wiki_downloader_java/src/application/Languages.java
+++ wiki_downloader_java/src/application/Languages.java
@@ -1,14 +1,3 @@
-/*
-    author: Amirul Umar Bin Pandai
-
-    Languages:
-    takes the name of the project and goes to an external wiki page
-    to return a list of all the supported languages, according to the
-    wiki page, in String format
-
-    input: String name of project
-    output: ArrayList of languages
-*/
 package application;
 
 import org.jsoup.Jsoup;
@@ -18,8 +7,21 @@ import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 
+/**
+ * @author Amirul Umar Bin Pandai
+ *
+ * Languages takes the name of the project and goes to an external wiki page
+ * to return a list of all the supported languages, according to the
+ * wiki page, in String format
+ *
+ */
 public class Languages {
 
+    /**
+     *
+     * @param project Name of the project.
+     * @return ArrayList of languages.
+     */
     public static ArrayList<String> getLanguagesFromProject(String project) {
         String html = "https://meta.wikimedia.org/wiki/Table_of_Wikimedia_projects";
         ArrayList<String> languages = new ArrayList<>();
@@ -27,31 +29,32 @@ public class Languages {
             Document doc = Jsoup.connect(html).get();
             Elements tableElements = doc.select("table.wikitable.sortable");
             Elements rows = tableElements.select("tr");
-			
+
+            project.toUpperCase();
             int column = 0;
             switch (project) {
-                case "Wikipedia":
+                case "WIKIPEDIA":
                     column = 3;
                     break;
-                case "Wiktionary":
+                case "WIKTIONARY":
                     column = 4;
                     break;
-                case "Wikibooks":
+                case "WIKIBOOKS":
                     column = 5;
                     break;
-                case "Wikinews":
+                case "WIKINEWS":
                     column = 6;
                     break;
-                case "Wikiquote":
+                case "WIKIQUOTE":
                     column = 7;
                     break;
-                case "Wikisource":
+                case "WIKISOURCE":
                     column = 8;
                     break;
-                case "Wikiversity":
+                case "WIKIVERSITY":
                     column = 9;
                     break;
-                case "Wikivoyage":
+                case "WIKIVOYAGE":
                     column = 10;
                     break;
                 default:
