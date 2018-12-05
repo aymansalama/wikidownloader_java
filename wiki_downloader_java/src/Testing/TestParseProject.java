335adb3da91494e8dc52d15ebcec87947b55168b
superlws1 <superlws1@hotmail.com>
2018-12-05 17:54:20 +0800

    testing output for automation testing

---
+++ wiki_downloader_java/src/Testing/TestParseProject.java
@@ -0,0 +1,13 @@
+package Testing;
+
+public class TestParseProject {
+
+    public static void execute() {
+
+//        Insert function here
+//        Load csv file
+//        Parse into arraylist
+//        Loop through all arraylist object
+//        Display similarity percentage
+    }
+}
