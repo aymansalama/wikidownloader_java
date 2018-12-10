6838c15d94ca5eb291701df500c3f812e8ce036f
kesaven8 <kesavenvulliamay@gmail.com>
2018-12-11 01:02:29 +0800

    Revert "Merge branch 'master' into Timestamp-issue"
    This reverts commit 4788214ed003881b01d841acc2dc82819f1cd2c7, reversing
    changes made to e7c7c422a9acf454dac670d27bcb10965becd447.

--- wiki_downloader_java/src/Testing/TestParseProject.java
+++ wiki_downloader_java/src/Testing/TestParseProject.java
@@ -1,25 +1,13 @@
 package Testing;
 
-import java.util.ArrayList;
+public class TestParseProject {
 
-public class TestParseProject extends Parser {
-
-    public static void main(String[] args)
-    {
-        execute();
-    }
-
-    private static void execute() {
-
+    public static void execute() {
 
 //        Insert function here
 //        Load csv file
 //        Parse into arraylist
 //        Loop through all arraylist object
 //        Display similarity percentage
-
-        String file = "project.json";
-
-        ArrayList<String> outputProjects = parseJSON(file);
     }
 }
